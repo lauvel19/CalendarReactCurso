f582e4663a476604701492857210d64856c46a5a
Laura Vélez <lgvelez_159@cue.edu.co>
2026-02-20 16:27:24 -0500

    correccion path

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
-app.use("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
